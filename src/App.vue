5f74f519ac62157f12970c25ae358f1abe06cf55
Ruma303 <matteoh8@hotmail.it>
2023-12-31 14:10:31 +0100

    modifica composables

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import Options from './components/Options.vue';
-import Compositions from './components/Compositions.vue';
-import Setup from './components/Setup.vue';
-import Reactive from './components/Reactive.vue';
 import WithComposables from './components/WithComposables.vue';
 
 </script>
 
 <template>
-    <Options></Options>
-    <Compositions></Compositions>
-    <Setup></Setup>
-    <Reactive></Reactive>
+
     <WithComposables />
 </template>
 
