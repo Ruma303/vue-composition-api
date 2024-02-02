be18bd8ad86d200c3ec03db4a32934887941208c
Ruma <matteoh8@hotmail.it>
2024-02-02 14:00:12 +0100

    computed

--- src/components/Reactive.vue
+++ src/components/Reactive.vue
@@ -4,7 +4,14 @@
         <input v-model="message" />
         <p>{{ message }}</p>
 
-        <h2>List Rendering</h2>
+        <h2>Grocery List</h2>
+        <form @submit.prevent="addItem">
+            <label for="new-item">add new item</label>
+            <input type="text" v-model.trim="newItem" id="new-item"
+                placeholder="Inserisci nuovo item...">
+        </form>
+        <p>{{ charactersCount }} / 20</p>
+        <p>Items counter: {{ itemsCount }}</p>
         <ul>
             <li v-for="({ id, label }, index) in items" :key="id">
                 {{ index }} - {{ label }}</li>
@@ -13,13 +20,31 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+
 let message = ref('Variabile reattiva');
 const items = ref([
     { id: 1, label: "coffee" },
     { id: 2, label: "sun glasses" },
     { id: 3, label: "black hat" }
 ]);
+
+    const newItem = ref('');
+    const addItem = () => {
+        if (newItem.value.length <= 20) {
+            items.value.push(
+                { id: items.value.length + 1, label: newItem.value });
+            newItem.value = '';
+        }
+    }
+
+    // Computed properties
+    const itemsCount = computed(() => {
+        return items.value.length;
+    });
+    const charactersCount = computed(() => {
+        return newItem.value.length;
+    });
 
 /* (function() {
     message = 'Nuovo valore'; // Cambia
@@ -28,13 +53,15 @@ const items = ref([
 setTimeout(()=> // Non cambia
 message = 'Valore cambiato', 3000); */
 
-(function () {
+/* (function () {
     message.value = 'Nuovo valore';
 })()
 
 setTimeout(() => {
     message.value = 'Valore cambiato dopo 3 secondi';
-}, 3000);
+}, 3000); */
+
+
 
 </script>
 
@@ -49,5 +76,13 @@ section {
     height: 100%;
     width: 100%;
     margin: 0 auto;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: .5em;
 }
 </style>
