747d40e47e46d81414223883cd2249eec57e6097
Ruma303 <matteoh8@hotmail.it>
2024-02-19 19:10:30 +0100

    aggiunto useOnline

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+import { useMouse, useOnline } from '@vueuse/core';
+const { x, y } = useMouse();
+const online = useOnline();
 </script>
 
 <template>
-    <main class="min-w-[300px] max-w-[900px] min-h-[800px] shadow-xl shadow-gray-300 flex flex-col gap-2 items-center py-4 mt-16 mx-auto rounded-lg">
+    <main
+        class="min-w-[300px] max-w-[900px] min-h-[800px] shadow-xl shadow-gray-300 flex flex-col gap-2 items-center py-8 mt-16 mx-auto rounded-lg relative">
+        <div class="absolute right-1 top-1 flex gap-4">
+            <p>Mouse coordinates: {{ x }} - {{ y }}</p>
+            <p :class="online ? 'text-emerald-500' : 'text-red-500'">
+                {{ online ? 'You are online 🟢' : 'You are offline 🔴' }}
+            </p>
+        </div>
         <h1 class="text-4xl mb-4">Composables</h1>
 
         <nav class="flex gap-2 items-center">
