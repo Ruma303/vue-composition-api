eee66ddf17a9895c5f257ef093c0bedf2b3877f4
Ruma <matteoh8@hotmail.it>
2024-12-14 11:16:33 +0100

    ok

--- src/components/Posts/Posts.vue
+++ src/components/Posts/Posts.vue
@@ -1,12 +1,11 @@
 <template>
-    <section>
+    <section class="posts">
         <h2>Posts</h2>
         <div>
-            <label for="">Find post</label>
             <input type="text" v-model="search" />
         </div>
         <ul>
-            <li v-for="post in posts" :key="post.id">
+            <li v-for="post in filteredPosts" :key="post.id">
                 <router-link :to="{ name: 'post', params: { id: post.id } }">
                     {{ post.id }}. {{ post.title }}
                 </router-link>
@@ -16,7 +15,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const search = ref('');
 
@@ -25,11 +24,26 @@ const posts = ref([
     { id: 2, title: 'Post 2' },
     { id: 3, title: 'Post 3' },
 ]);
+
+const filteredPosts = computed(() => {
+    if (!search.value) {
+        return posts.value;
+    }
+    return posts.value.filter((post) => post.title.toLowerCase().includes(search.value.toLowerCase()));
+});
+
+
 </script>
 
 <style scoped>
 h2 {
     text-align: center;
+}
+
+.posts {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 }
 
 ul {
